3bb06ad509e4f4784c5ed2b9aed8ae77da1011dc
mbbm976 <mbbm976@gmail.com>
2026-05-18 04:33:39 +0000

    Use real Tailwind PostCSS config

--- frontend/postcss.config.cjs
+++ frontend/postcss.config.cjs
@@ -1,12 +1,6 @@
-const stripTailwindDirectivesUntilDependencyIsAvailable = {
-  postcssPlugin: 'strip-tailwind-directives-until-dependency-is-available',
-  AtRule(atRule) {
-    if (atRule.name === 'tailwind') {
-      atRule.remove()
-    }
-  },
-}
-
 module.exports = {
-  plugins: [stripTailwindDirectivesUntilDependencyIsAvailable],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
